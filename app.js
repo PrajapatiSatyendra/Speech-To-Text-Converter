88fc0080902cd796c11530ce58d9562d6ee6f7b7
PrajapatiSatyendra <sat87866@gmail.com>
2023-02-03 21:15:24 +0530

    "update4"

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ if (process.env.NODE_ENV == 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
-app.listen(PORT,()=>{
+app.listen(8000,()=>{
     console.log("listening at port 8000")
 })
